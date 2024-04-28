7c92a8439c38de4f4da87802dcf0af6480dfa889
yun-cheng <yuncheng.dev@gmail.com>
2024-04-28 23:03:33 +0800

    feat(PricingCard): add price ui

--- src/components/PricingCard/PricingCard.tsx
+++ src/components/PricingCard/PricingCard.tsx
@@ -2,17 +2,19 @@ import { Card, CardContent } from 'components/ui/Card'
 import Divider from 'components/ui/Divider'
 import type { ReactElement } from 'react'
 import PricingCardPageViews from './PricingCardPageViews'
+import PricingCardPrice from './PricingCardPrice'
 import PriceCardSlider from './PricingCardSlider'
 
 export default function PricingCard(): ReactElement {
 	return (
 		<Card className='mx-auto max-w-[540px]'>
 			<CardContent className='px-6 pt-[35px] sm:p-12'>
-				<div className='flex flex-col items-center sm:flex-row sm:items-start'>
+				<div className='flex flex-col items-center sm:flex-row sm:items-start sm:justify-between'>
 					<PricingCardPageViews className='sm:mt-3 sm:-translate-x-[3px]' />
 					<PriceCardSlider className='mt-6 sm:hidden' />
+					<PricingCardPrice className='mt-[30px] sm:mt-0' />
 				</div>
-				<PriceCardSlider className='hidden sm:block' />
+				<PriceCardSlider className='mt-2 hidden sm:block' />
 			</CardContent>
 			<Divider />
 		</Card>
---
+++ src/components/PricingCard/PricingCardPrice.tsx
@@ -0,0 +1,37 @@
+import type { HTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+import cn from 'utils/cn'
+
+const PricingCardPrice = forwardRef<
+	HTMLDivElement,
+	HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+	<div ref={ref} className={className} {...props}>
+		<div
+			className={cn('-translate-x-px', 'flex items-center gap-[10px] sm:gap-2')}
+		>
+			<span className='text-[32px] font-extrabold leading-none -tracking-[.8px] text-dark-desaturated-blue sm:text-[40px] sm:-tracking-[1px]'>
+				$16.00
+			</span>
+			<span
+				className={cn(
+					'translate-y-px sm:translate-y-0',
+					'text-[14px] sm:text-[16px]'
+				)}
+			>
+				/ month
+			</span>
+		</div>
+		<div
+			className={cn(
+				'mt-[2px] flex justify-end sm:mt-[7px]',
+				'whitespace-pre-wrap text-[10px] tracking-[.2px] sm:text-[14px] sm:-tracking-[.4px]'
+			)}
+		>
+			{`or ~32 CRO `}
+			<span className='-translate-x-px sm:translate-x-0'>/ month</span>
+		</div>
+	</div>
+))
+
+export default PricingCardPrice
